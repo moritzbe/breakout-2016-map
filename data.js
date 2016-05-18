57e5e22a933f656ccc441625a044ce7c06d9cf69
moritzbe <moritzberthold@hotmail.com>
2016-05-18 17:09:55 +0200

    first map draft for breakout2016

---
+++ data.js
@@ -0,0 +1,114 @@
+var data = [{
+    id: 1,
+    name: 'Erstes Team',
+    event: {
+      id: 1,
+      title: 'Breakout München 2016',
+      date: 1464912000,
+      city: 'München',
+      startingLocation: {latitude: 48.13743, longitude: 11.57549},
+      duration: 36
+    },
+    description: 'Geile Sache',
+    members: [
+      {
+        firstname: 'Keno',
+        lastname: 'Dreßel',
+        id: 1,
+        participant: [Object],
+        profilePic: [Object],
+        roles: [Object],
+        blocked: true
+      },
+      {
+        firstname: 'Moritz',
+        lastname: 'Berthold',
+        id: 1,
+        participant: [Object],
+        profilePic: [Object],
+        roles: [Object],
+        blocked: true
+      }],
+    profilePic: {id: 21, type: 'IMAGE', uploadToken: null, sizes: []},
+    locations: [
+      {latitude: 48.144852, longitude: 17.065720},
+      {latitude: 48.143485, longitude: 17.065441},
+      {latitude: 44.842329, longitude: 29.424014}
+    ],
+    invoiceId: 1
+  }, {
+    id: 2,
+    name: 'Zweites Team',
+    event: {
+      id: 2,
+      title: 'Breakout München 2016',
+      date: 1464912000,
+      city: 'München',
+      startingLocation: {latitude: 48.13743, longitude: 11.57549},
+      duration: 36
+    },
+    description: 'Hornhaut',
+    members: [
+      {
+        firstname: 'Ken',
+        lastname: 'D',
+        id: 2,
+        participant: [Object],
+        profilePic: [Object],
+        roles: [Object],
+        blocked: true
+      },
+      {
+        firstname: 'Mo',
+        lastname: 'Be',
+        id: 2,
+        participant: [Object],
+        profilePic: [Object],
+        roles: [Object],
+        blocked: true
+      }],
+    profilePic: {id: 21, type: 'IMAGE', uploadToken: null, sizes: []},
+    locations: [
+      {latitude: 48.024359, longitude: 7.244407},
+      {latitude: 42.932577, longitude: -2.397254},
+      {latitude: 39.693748, longitude: -8.826598}
+    ],
+    invoiceId: 2
+  }, {
+    id: 3,
+    name: 'Drittes Team',
+    event: {
+      id: 3,
+      title: 'Breakout Berlin 2016',
+      date: 1464912000,
+      city: 'Berlin',
+      startingLocation: {latitude: 52.512601, longitude: 13.321778},
+      duration: 36
+    },
+    description: 'Gesichtsgulasch',
+    members: [
+      {
+        firstname: 'Roberto',
+        lastname: 'D',
+        id: 3,
+        participant: [Object],
+        profilePic: [Object],
+        roles: [Object],
+        blocked: true
+      },
+      {
+        firstname: 'Julia',
+        lastname: 'S',
+        id: 3,
+        participant: [Object],
+        profilePic: [Object],
+        roles: [Object],
+        blocked: true
+      }],
+    profilePic: {id: 21, type: 'IMAGE', uploadToken: null, sizes: []},
+    locations: [
+      {latitude: 54.255022, longitude: 16.485900},
+      {latitude: 55.018009, longitude: 24.835509}
+    ],
+    invoiceId: 2
+  }];
---
+++ map.js
@@ -0,0 +1,114 @@
+function initMap() {
+  var munich = new google.maps.LatLng(48.150618, 11.581317);
+  var berlin = new google.maps.LatLng(52.512601, 13.321778);
+  var teamarray = [];
+
+
+  var mapOptions = {
+      center: munich,
+      zoom: 4,
+      styles: mapstyle(),
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      scrollwheel: false,
+    };
+
+  this.map = new google.maps.Map(document.getElementById("map"),
+        mapOptions);
+
+  var teams = getDataJson();
+  drawRoute(teams);
+
+  // var marker = new google.maps.Marker({
+  //   position: munich,
+  //   map: map,
+  //   title: 'Breakout Map'
+  // });
+};
+
+
+//Map Night-Daystyle
+function mapstyle (){
+  var date = new Date();
+  var day_map = [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}];
+  var night_map = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#021019"}]}];
+  var hour = date.getHours();
+  if(6 < hour && hour < 21) {
+    return day_map;
+  } else { 
+  return night_map;    
+  }
+};
+
+// Get all team-data
+var getDataJson = function (){
+  console.log(data);
+  return data;
+};
+
+// Draw the route of each team
+var drawRoute = function (teams){
+  teams.forEach(function (team){
+      console.log(team);
+      var startingposition = new google.maps.LatLng(team.event.startingLocation.latitude, team.event.startingLocation.longitude);
+      var route = [startingposition];          
+      var i;
+
+      for (i = 0; i < team.locations.length; i++) {
+        route.push(new google.maps.LatLng(team.locations[i].latitude, team.locations[i].longitude));
+      }
+
+      marker(team.locations, team, map);
+
+      var flightPath = new google.maps.Polyline({
+        path:route,
+        strokeColor: getColor(team),
+        strokeOpacity: 1.0,
+        strokeWeight: 3
+      });  
+      flightPath.setMap(map); 
+    });
+};
+
+
+//Marker on last position
+function marker(coords, team, map) {
+  var marker;
+  marker = new google.maps.Marker({
+    position: new google.maps.LatLng(coords[coords.length-1].latitude, coords[coords.length-1].longitude),
+    map: map
+  });
+
+  var infowindow = new google.maps.InfoWindow({
+      content: makeContent(team)
+  });
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
+};
+
+
+
+//Window that shows on a marker
+function makeContent(team) {
+  var contentstring = '<div id="content">'+
+    '<div id="siteNotice">'+
+    '</div>'+
+    '<h4 id="firstHeading" class="firstHeading">' + team.name + '</h4>'+
+    '<p><b>' + team.members[0].firstname + ' und ' + team.members[1].firstname + ', aus ' + team.event.city + '</b></p></div>'
+  console.log(contentstring);  
+  return contentstring;
+};
+
+
+
+//Select Teamcolor for Munich/Berlin
+function getColor(team) {
+  var colorlist = {
+    "Munich": '#FF0000',
+    "Berlin": '#FFFF00' 
+  };
+  return colorlist[team.event.city];
+}
+
+
+
